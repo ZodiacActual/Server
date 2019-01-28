75238432767a5c14f84a1536a0e9dccb1b35b3a2
YoonJeongMin <gkszm636@gmail.com>
2019-01-28 17:46:02 +0900

    added encrypt

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,9 @@
 import java.sql.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.text.*;
+import java.security.*;
 
 public class Server {
         static {
@@ -17,10 +22,30 @@ public class Server {
             );
             Statement stmt = con.createStatement();
             StringBuffer sb = new StringBuffer();
+            ServerSocket listener = new ServerSocket(9999);
+            System.out.println("사용자 접속 대기중....");
+            Socket socket = listener.accept();
+            System.out.println("사용자가 접속하였습니다. ");
+            System.out.println("사용자 IP : " + socket.getInetAddress());
+            OutputStream os = socket.getOutputStream();
+            InputStream is =  socket.getInputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            ObjectInputStream ios = new ObjectInputStream(is);
+            int input = 0;
+            String inputString = null;
+            String outputString = null;
+            boolean exit = false;
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(1024);
+            KeyPair keypair = kpg.genKeyPair();
+            PublicKey pubkey = keypair.getPublic();
+            PrivateKey prikey = keypair.getPrivate();
+            while (true) {
 
-        } catch(SQLException sqle) {
+            }
+        } catch(SQLException | IOException | NoSuchAlgorithmException exception) {
             System.out.println("DB Connection Error");
-            sqle.printStackTrace();
+            exception.printStackTrace();
         }
     }
 }
